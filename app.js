f8d76f8db7ec6bd0a5ce75cb29c91b62f4b4a294
Aya-AbuNajm <ayaabunajm@gmail.com>
2021-04-26 23:54:37 +0300

    play

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ let userName =prompt('Enter your name ?');
 let Age =prompt('Enter your age ');
 let maj =prompt('Enter your major');
 let brsis =prompt('how many brothers and sisters do you have?');
+//document.getElementById("div1").innerHTML = brsis;
+//var xx = document.getElementById("div11").innerHTML;
+//alert(xx);
 
-
+// div11.innerHTML += '<p style=text-align: center;color: rgb(238, 225, 110);background-color: rgb(90, 90, 72);>
 
 
 alert('welcome ' + userName );
@@ -12,12 +15,16 @@ alert(maj +' it\'s amazing major');
 alert( 'you have  '+brsis+'  of brothers and sisters' );
 
 
+
 console.log ('User Name is  ' + userName);
 console.log ('Age  '+ Age);
 console.log('major is  ' + maj);
 
-document.write(userName);
+document.write(userName +'\n');
+document.write(Age +'\n');
+document.write(maj +'\n');
+document.write(brsis +'\n');
 
-// var div = document.getElementById('divID'); div.innerHTML += 'Extra stuff';
+
 
 
