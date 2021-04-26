5680e3183c87c66fef9565675976435aadfb7b3f
Aya-AbuNajm <ayaabunajm@gmail.com>
2021-04-26 08:45:19 +0300

    lab01

---
+++ app.js
@@ -0,0 +1,23 @@
+let userName =prompt('Enter your name ?');
+let Age =prompt('Enter your age ');
+let maj =prompt('Enter your major');
+let brsis =prompt('how many brothers and sisters do you have?');
+
+
+
+
+alert('welcome ' + userName );
+alert(Age +' is the best age so enjoy');
+alert(maj +' it\'s amazing major');
+alert( 'you have  '+brsis+'  of brothers and sisters' );
+
+
+console.log ('User Name is  ' + userName);
+console.log ('Age  '+ Age);
+console.log('major is  ' + maj);
+
+document.write(userName);
+
+// var div = document.getElementById('divID'); div.innerHTML += 'Extra stuff';
+
+
